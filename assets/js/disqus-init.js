273675c8dc53ff940eeb8f64591e680003202160
Raffaele Carelle <raffaele.carelle@algoritma.it>
2020-01-22 15:56:18 +0100

    fix js

--- assets/js/disqus-init.js
+++ assets/js/disqus-init.js
@@ -1,6 +1,5 @@
-var disqus_shortname = document.currentScript.getAttribute('disqus_username');
-
 (function() { // DON'T EDIT BELOW THIS LINE
+    var disqus_shortname = document.currentScript.getAttribute('disqus_username');
     var d = document, s = d.createElement('script');
     s.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
     s.setAttribute('data-timestamp', +new Date());
